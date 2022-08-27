1de3ef063feb11a8dc219f054f34de10f9355e17
fishaction <micrasoft.bf4@gmail.com>
2022-08-28 00:09:52 +0900

    Devicesのリストアップを実装

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,49 @@
 <template>
   <splitpanes style="height: 100vh">
-    <pane size="80">
+    <pane size="78">
       <splitpanes horizontal>
         <pane size="75"><Map></Map></pane>
         <pane size="25">ターミナル</pane>
       </splitpanes>
     </pane>
-    <pane size="20">
+    <pane size="22">
       <el-tabs :tab-position="tabPosition" style="height: 100%">
-        <el-tab-pane label="Devices">Devices</el-tab-pane>
+        <el-tab-pane style="padding: 0px 0px 0px 10px;" label="Devices">
+          <h3 style="margin-bottom:0px;">Devices</h3>
+          <el-divider />
+          <div v-for="device in devices" :key="device.id">
+            <span>{{ device.name }}</span><br />
+            <table class="deviceDetectionTable">
+              <tbody>
+                <tr>
+                  <td>バッテリー残量</td>
+                  <td>{{ device.remaining_bat }}%</td>
+                </tr>
+                <tr>
+                  <td>最終検出</td>
+                  <td>20XX-XX-XX</td>
+                </tr>
+                <tr>
+                  <td>ID</td>
+                  <td>{{device.id}}</td>
+                </tr>
+              </tbody>
+            </table>
+            <el-divider />
+          </div>
+        </el-tab-pane>
         <el-tab-pane label="Logs">Logs</el-tab-pane>
-      </el-tabs></pane
-    >
+      </el-tabs>
+    </pane>
   </splitpanes>
 </template>
 
 <script lang="ts">
 import Map from "@/components/Map.vue";
+import { affixProps } from "element-plus";
 import { Splitpanes, Pane } from "splitpanes";
 import "splitpanes/dist/splitpanes.css";
+import axios from 'axios'
 
 export default {
   components: {
@@ -26,10 +51,16 @@ export default {
     Pane,
     Map,
   },
-  data(){
-    return{
+  data() {
+    return {
       tabPosition: 'right',
+      devices: [],
     }
+  },
+  mounted(){
+    axios
+      .get('/api/devices')
+      .then(response => (this.devices = response.data));
   }
 };
 </script>
@@ -39,22 +70,42 @@ body {
   margin: 0%;
 }
 
-.splitpanes--vertical > .splitpanes__splitter {
+.el-divider--horizontal {
+  margin: 10px 0 !important;
+}
+
+.splitpanes--vertical>.splitpanes__splitter {
   min-width: 3px;
   background: #222;
   transition: 0.1s;
 }
-.splitpanes--vertical > .splitpanes__splitter:hover {
+
+.splitpanes--vertical>.splitpanes__splitter:hover {
   background: #409eff;
   transform: scale(2.4, 1);
 }
-.splitpanes--horizontal > .splitpanes__splitter {
+
+.splitpanes--horizontal>.splitpanes__splitter {
   min-height: 3px;
   background: #222;
   transition: 0.1s;
 }
-.splitpanes--horizontal > .splitpanes__splitter:hover {
+
+.splitpanes--horizontal>.splitpanes__splitter:hover {
   background: #409eff;
   transform: scale(1, 2.4);
+}
+
+.deviceDetectionTable {
+  border: none;
+  font-size: 0.75rem;
+}
+
+.deviceDetectionTable>tbody>tr>td:nth-child(n+2) {
+  padding-left: 8px;
+  word-break: break-all;
+}
+.deviceDetectionTable>tbody>tr>td:nth-child(1) {
+  white-space: nowrap;
 }
 </style>
